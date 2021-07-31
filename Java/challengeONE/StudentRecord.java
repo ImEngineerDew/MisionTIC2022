03bc5fdd6c702f3f6c3933bedc404df3cb8b3c0f
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-07-31 18:55:48 -0500

    Changing the type of dat from the sum variable

--- Java/challengeONE/StudentRecord.java
+++ Java/challengeONE/StudentRecord.java
@@ -4,23 +4,26 @@ import java.util.Scanner;
 public class StudentRecord
 {
     private static Scanner object = new Scanner(System.in);
-    static Integer array[],i,amount,sum = 0;
-    static Double average;
+    static Integer i ,sum = 0;
+    static Double average, array[];
 
     static double quantityOfData(Integer amount)
     {
-        array = new Integer[amount];
+        array = new Double[amount];
         System.out.println("Size of the array: "+amount);
         for (i=0; i<amount; i++)
         {
             System.out.print("Data of the position"+i+": ");
-            array[i] = object.nextInt();
+            array[i] = object.nextDouble();
         }
         return amount;
     }
 
     public static void main(String args[])
     {
+        double sumatory = 0;
+
+        sumatory = (double)sum;
         System.out.print("Please write the amount of data: ");
         Integer amnt = object.nextInt();
 
@@ -28,11 +31,10 @@ public class StudentRecord
 
         for (int j=0; j<amnt;j++)
         {
-            sum+=array[j];
-            double resultSum = (double)sum;
-            average = resultSum/amnt;
+            sumatory+=array[j];
+            average = sumatory/amnt;
         }
-        System.out.println("Sum: "+sum);
+        System.out.println("Sum: "+sumatory);
         System.out.print("Average: "+average);
     }
 }
