57061e121f841f1c9c29c36047d5c91ac257349f
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-07-31 18:36:43 -0500

    Improve the sum operations of the arrays

--- Java/challengeONE/StudentRecord.java
+++ Java/challengeONE/StudentRecord.java
@@ -4,22 +4,29 @@ import java.util.Scanner;
 public class StudentRecord
 {
     private static Scanner object = new Scanner(System.in);
-    static Integer amount, i;
+    static Integer array[],i,amount,sum = 0;
+    static double quantityOfData(Integer amount)
+    {
+        array = new Integer[amount];
+        System.out.println("Size of the array: "+amount);
+        for (i=0; i<amount; i++)
+        {
+            System.out.print("Data of the position"+i+": ");
+            array[i] = object.nextInt();
+        }
+        return amount;
+    }
 
     public static void main(String args[])
     {
         System.out.print("Please write the amount of data: ");
-        amount = object.nextInt();
-        Integer array[] =  new Integer[amount];
+        Integer amount = object.nextInt();
+        quantityOfData(amount);
 
-        for(i=0; i<amount; i++)
+        for (int j=0; j<amount;j++)
         {
-            System.out.print("Please write the data: ");
-            array[i] = object.nextInt();
+            sum+=array[j];
         }
-        for (i=0; i<amount;i++)
-        {
-            System.out.println("Data: "+array[i]);
-        }
+        System.out.println("Sum: "+sum);
     }
 }
