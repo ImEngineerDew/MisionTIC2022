cd1776ce6ab30e93e08976aa097776e3a4311e88
Dewin Acosta <dewin.acosta9207@gmail.com>
2021-07-31 18:47:40 -0500

    I've finished the commits on that branch

--- Java/challengeONE/StudentRecord.java
+++ Java/challengeONE/StudentRecord.java
@@ -5,6 +5,8 @@ public class StudentRecord
 {
     private static Scanner object = new Scanner(System.in);
     static Integer array[],i,amount,sum = 0;
+    static Double average;
+
     static double quantityOfData(Integer amount)
     {
         array = new Integer[amount];
@@ -20,13 +22,17 @@ public class StudentRecord
     public static void main(String args[])
     {
         System.out.print("Please write the amount of data: ");
-        Integer amount = object.nextInt();
-        quantityOfData(amount);
+        Integer amnt = object.nextInt();
 
-        for (int j=0; j<amount;j++)
+        quantityOfData(amnt);
+
+        for (int j=0; j<amnt;j++)
         {
             sum+=array[j];
+            double resultSum = (double)sum;
+            average = resultSum/amnt;
         }
         System.out.println("Sum: "+sum);
+        System.out.print("Average: "+average);
     }
 }
